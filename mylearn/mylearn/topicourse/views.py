8c6e4880036522074d6b223b1e0bd17f1e20d1b7
root <root@eric-ubuntu.(none)>
2013-12-02 22:16:17 +0800

    implement fake interface: getUserTopicourses

--- mylearn/mylearn/topicourse/views.py
+++ mylearn/mylearn/topicourse/views.py
@@ -1,1 +1,27 @@
 # Create your views here.
+
+# [] is list
+# {} is dictionary
+def getUserTopicourses(user_email, get_type, num):
+    topicourses = {}
+
+    topicourses['userTopicoursesList'] = []
+    for j in range(0, 3):
+        topicourse = {}
+        topicourse['userTopicoursesTimeStamp'] = 0
+        topicourse['topicourseCreatorUserID'] = 10000
+        topicourse['topicourseTitle'] = 'topicourseTitleForTest'
+        topicourse['topicoursePath'] = '/redirect/url/'
+
+        topicourse['userTopiquizList'] = []
+        for i in range(0, 3):
+            topiquiz = {}
+            topiquiz['userTopiquizTimeStamp'] = i
+            topiquiz['userTopiquizResults'] = 80 + i
+            topicourse['userTopiquizList'].append(topiquiz)
+
+        topicourses['userTopicoursesList'].append(topicourse)
+
+
+    return topicourses
+
