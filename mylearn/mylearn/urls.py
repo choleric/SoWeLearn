c06fbbcc28474d141968f82029319bc97fd79e93
meichen91 <meichenlu91@gmail.com>
2013-12-28 12:33:37 +0000

    urlsettings for social account

--- mylearn/mylearn/urls.py
+++ mylearn/mylearn/urls.py
@@ -36,7 +36,7 @@ urlpatterns += patterns(PROJECT_APP_PREFIX + '.tuition_map.views',
 
 urlpatterns += patterns('',
     (r'^accounts/', include(get_project_app_qulified_name('allauth_override_template.urls'))),
-    (r'^accounts/', include(get_project_app_qulified_name('allauth_socialaccount.urls'))),
+    (r'^accounts/social/', include(get_project_app_qulified_name('allauth_socialaccount.urls'))),
     (r'^accounts/', include('allauth.urls')),
 )
 
