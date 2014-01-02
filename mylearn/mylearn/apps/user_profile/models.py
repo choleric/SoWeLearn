688a2dbb2f7e1465941f3542aa3d9c6bd6868cda
meichen91 <meichenlu91@gmail.com>
2014-01-02 12:26:06 +0000

    profile models

--- mylearn/mylearn/apps/user_profile/models.py
+++ mylearn/mylearn/apps/user_profile/models.py
@@ -1,63 +1,33 @@
-from mongoengine import *
 from django.db import models
-from djangotoolbox.fields import EmbeddedModelField
+from djangotoolbox.fields import EmbeddedModelField, ListField
 
 # Create your models here.
 
-class userPersonalProfile(EmbeddedDocument):
-    aboutUserQuote = StringField(max_length=120)
-    userEducationCredentials = StringField(max_length=120)
-    userWorkCredentials = StringField(max_length=120)
-    userLocation = StringField(max_length=120)
-
-class User(Document):
-    userEmail = StringField(max_length=120, required=True,unique=True)
-    userFirstName = StringField(max_length=50)
-    userLastName = StringField(max_length=50)
-    #userPersonalProfile=EmbeddedDocumentField(userPersonalProfile)
-
-    def user_signup(self, userEmail, userFirstName, userLastName):
-        self.userEmail = userEmail
-        self.userFirstName = userFirstName
-        self.userLastName = userLastName
-        self.create()
-#
-class UserVerified(EmbeddedDocument):
-    IsVerified = BooleanField(default=False)
-    verifiedTimeStamp = LongField()
-    verifiedStaffId = LongField()
-
-    meta = {'allow_inheritance': True}
-
 class UserVerified(models.Model):
-    IsVerified = BooleanField(default=False)
-    verifiedTimeStamp = LongField()
-    verifiedStaffId = LongField()
+    IsVerified = models.BooleanField(default=False)
+    verifiedTimeStamp = models.DateTimeField(auto_now=True)
+    verifiedStaffId = models.BigIntegerField()
 
 class UserEducationCredential(UserVerified):
-    userEducationInfo = StringField()
+    userEducationInfo = models.CharField(max_length=100)
 
 class UserWorkCredential(UserVerified):
-    userWorkInfo = StringField()
+    userWorkInfo = models.CharField(max_length=100)
 
-
+class TutorHourlyRate(models.Model):
+    tutorTuitionAverageHourlyRateMiddleSchool = models.PositiveSmallIntegerField(blank=True)
+    tutorTuitionAverageHourlyRateHighSchool = models.PositiveSmallIntegerField(blank=True)
+    tutorTuitionAverageHourlyRateCollege = models.PositiveSmallIntegerField(blank=True)
 
 class UserPersonalProfile(models.Model):
     userID = models.BigIntegerField(primary_key=True)
-    skypeID = models.CharField(max_length=20)
-    aboutUserQuote = models.CharField(required = False)
-
-
-"""
-    aboutUserQuote = StringField()
-    userEducationCredential = ListField(
-                                EmbeddedDocumentField(UserEducationCredential))
-    userWorkCredential = ListField(EmbeddedDocumentField(UserWorkCredential))
-    userLocation = StringField()
-
-    # only for tutor
-    tutorTuitionTopics = StringField()
-    tutorTuitionAverageHourlyRateMiddleSchool = LongField()
-    tutorTuitionAverageHourlyRateHighSchool = LongField()
-    tutorTuitionAverageHourlyRateCollege = LongField()
-"""
+    skypeID = models.CharField(max_length=20, blank=True)
+    aboutUserQuote = models.CharField(blank=True, max_length=100)
+    userEducationCredential = ListField(EmbeddedModelField('UserEducationCredential'), blank=True)
+    userWorkCredential = ListField(EmbeddedModelField('UserWorkCredential'), blank=True)
+    userLocation = models.CharField(max_length=50)
+    #whether user is verified as a tutor
+    verifiedTutor= models.BooleanField(default=False)
+    #Only available if user is verified as a tutor
+    tutorTuitionTopics = ListField()
+    tutorTuitionAverageHourlyRate = EmbeddedModelField('TutorHourlyRate')
