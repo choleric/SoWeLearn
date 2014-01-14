5bbfb07e17d12f7f3abefc88dae83467bb6b83bd
zhouweicsu <zzlyzhouwei@gmail.com>
2014-01-14 22:54:56 +0800

    login parts done

--- static/js/userinfo.js
+++ static/js/userinfo.js
@@ -2,14 +2,18 @@ define(function(require,exports,module){
 	var $ = require("libs/jquery-1.7.2");
     require('libs/jquery.cookie')($);
     require('libs/jquery.tipsy')($);
+
     var tools = require('tools');
+    var validate = require('validate');
 	
 	tools.getToken();
+	removeErrorHint();
 
 	//login
 	$(".login .loginBtn").bind("click",function(){
 		event.stopPropagation();//阻止冒泡
 		login();
+		removeErrorHint();
 		return false;
 	});
 
@@ -28,12 +32,65 @@ define(function(require,exports,module){
 		option.type = "post";
 		option.url = "/accounts/login/";
 		option.error = displayLoginErrorInfo;
+		option.success = loginSuccess;
 		tools.sendAjaxRequest(option,$.cookie("_t"));
 	}
 
 	function displayLoginErrorInfo(data){
-		console.log("login error...");
+		console.log(data.responseText);
 	}
+	function loginSuccess(data){
+		console.log(data.location);
+		if(data.location){
+			window.location.href = data.location;
+		}
+		var errorCode = data.c;
+		if(errorCode == "212"){
+			var d = data.d;
+			for(var i = 0, l = d.length; i < l; i++){
+				if(d[i]=="0"){
+					$(".J_emailError").val("Your Email Address is incorrectly formatted.").show();
+					$("#loginEmail").addClass('error');
+				}else if(d[i]=="1"){
+					$("#loginPassword").addClass('error');
+					$(".J_passwordError").val("Sorry,wrong password.").show();
+				}
+			}
+		}else if(errorCode == "211"){
+			$("#loginPassword").addClass('error');
+			$(".J_passwordError").val("Sorry,wrong password.").show();
+		}
+	}
+	function removeErrorHint(){
+		// $(".loginForm input[type='text']").each(function(index){
+		// 	$("this").bind('focus', function(event) {
+		// 		$(this).removeClass('error');
+		// 		$(this).next().hide();
+		// 	});
+		// });
+		$("#loginEmail").bind('focus', function(event) {
+			$("#loginEmail").removeClass('error');
+			$(".J_emailError").val("Your Email Address is incorrectly formatted.").hide();
+		});
+		$("#loginPassword").bind('focus', function(event) {
+			$("#loginPassword").removeClass('error');
+			$(".J_passwordError").val("Sorry,wrong password.").hide();
+		});
+	}
+	(function validateLoginInput(){
+		$("#loginEmail").bind('blur', function(event) {
+			if(!validate.isEmail(this.value)){
+				$(".J_emailError").val("Your Email Address is incorrectly formatted.").show();
+				$("#loginEmail").addClass('error');
+			}
+		});
+		$("#loginPassword").bind('blur', function(event) {
+			if($.trim(this.value) == "" || $.trim(this.value).length <6 ){
+				$(".J_passwordError").val("Your password must be at least 6 characters long.").show();
+				$("#loginPassword").addClass('error');
+			}
+		});	
+	})();
 
 	//signup
 	$(".signup .J_signupBtn").bind('click', function(event) {
@@ -50,9 +107,59 @@ define(function(require,exports,module){
 		option.type = "post";
 		option.url = "/accounts/signup/";
 		option.error = displaySignupErrorInfo;
+		option.success = signupSuccess;
 		tools.sendAjaxRequest(option,$.cookie("_t"));
 	}
-	function displaySignupErrorInfo(){
-		console.log("sign up error ....");
+	function displaySignupErrorInfo(data){
+		console.log(data);
+	}
+	function signupSuccess(data){
+		console.log(data.location);
+		if(data.location){
+			window.location.href = data.location;
+		}
+		var errorCode = data.c;
+		if(errorCode == "201"){
+			var d = data.d;
+			for(var i = 0, l = d.length; i < l; i++){
+				switch(d[i]){
+					case 0:
+						$(".J_emailSignError").val("Your Email Address is incorrectly formatted.").show();
+						$("#signUpEmail").addClass('error');
+						break;
+					case 1:
+						$(".J_passwordSignError1").val("Your password must be at least 6 characters long.").show();
+						$("#signUpPassword1").addClass('error');
+						break;
+					case 2:
+						$(".J_passwordSignError2").val("Your password must be at least 6 characters long.").show();
+						$("#signUpPassword2").addClass('error');
+						break;
+					case 3:
+						$(".J_firstNameError").val("Your password must be at least 6 characters long.").show();
+						$("#signFirstName").addClass('error');
+						break;
+					case 4:
+						$(".J_lastNameError").val("Your password must be at least 6 characters long.").show();
+						$("#signLastName").addClass('error');
+						break;
+				}
+			}
+		}
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- static/js/validate.js
+++ static/js/validate.js
@@ -1,12 +1,13 @@
 define(function(require,exports,module){
 	var $ = require("libs/jquery-1.7.2");
 
+	var validate = {};
 	/**
 	 * 验证是否为正确的邮箱格式
 	 * return 
 	 *    true or false
 	 */
-	module.isEmail = function(email){
+	validate.isEmail = function(email){
 	    var re_email = new RegExp("^([a-zA-Z0-9]+[_|\_|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$");
 	    if(re_email.test(email)){
 	        return true; 
@@ -14,4 +15,7 @@ define(function(require,exports,module){
 	        return false;
 	    }
 	}
+
+
+    module.exports = validate;
 });
